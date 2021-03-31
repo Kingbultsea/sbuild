58197602318d5582a2936bf4b5a97028c05823df
wjh123 <894275157@qq.com>
2021-03-31 14:57:06 +0800

    cm-1

---
+++ file-vue/test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class='test'>123456</div>
+</template>
+
+<script>
+    export default {
+      name: "123",
+      data() {
+        return {
+          a: 2
+        }
+      }
+    }
+</script>
+
+<style scoped lang="scss">
+    .test {
+        color: red
+    }
+</style>
