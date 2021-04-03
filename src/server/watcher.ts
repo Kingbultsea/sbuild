3c7b2527da0f25d3f5fb16bbafb400944549ac67
wjh123 <894275157@qq.com>
2021-04-03 12:58:20 +0800

    add ignore

--- src/server/watcher.ts
+++ src/server/watcher.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import chokidar from 'chokidar'
 import { parseSFC } from './vueCompiler'
+// @ts-ignore
 import hash_sum from 'hash-sum'
 import { SFCBlock } from '@vue/compiler-sfc'
 
